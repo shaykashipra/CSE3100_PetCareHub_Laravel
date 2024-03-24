23b0a7e59b8e5a14f353f45f2d0db9d046440084
Shayka Shipra <triviatruism@gmail.com>
2024-03-24 23:58:30 +0600

    login signup contact us design views components

---
+++ resources/views/components/input-error.blade.php
@@ -0,0 +1,11 @@
+@props(['messages'])
+
+@if ($messages)
+
+    <ul {{ $attributes->merge(['class' => 'text-danger ps-0 list-unstyled']) }}>
+        @foreach ((array) $messages as $message)
+            <li>{{ $message }}</li>
+        @endforeach
+    </ul>
+  
+@endif
